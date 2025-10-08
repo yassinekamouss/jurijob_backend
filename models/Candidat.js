319922235d64be7d0849dac7c88304316056382b
ahyahya1616 <ahmaneyahya10@gmail.com>
2025-10-08 19:41:21 +0100

    ajout de Demande model et modification de Candidat model

--- models/Candidat.js
+++ models/Candidat.js
@@ -3,17 +3,20 @@ const mongoose = require("mongoose");
 const candidatSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    posteActuel: { type: String, required: true },
     niveauExperience: { type: String, required: true },
     formationJuridique: { type: String, required: true },
     specialisations: { type: [String], default: null },
     domainExperiences: { type: [String], default: null },
-     langues: [
+    langues: [
       {
         nom: { type: String, required: true },  
         niveau: { type: String, required: true }
       }
     ],
-    typeTravail: {  type: String,  required: true},
+    typeTravail: {  type: String,  required: true},// Ex : "emploi", "stage"
+    modeTravail: { type: String, required: true },
+    villesTravail: { type: [String], required: true },
     imageUrl: { type: String , default: null }
   },
   { timestamps: true }
