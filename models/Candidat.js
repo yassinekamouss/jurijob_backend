721e156316a552829b6d9c044c850f245723fb44
Votre Nom <mohammedsalhisam@gmail.com>
2025-10-15 20:11:34 +0100

    update : sign up fields

--- models/Candidat.js
+++ models/Candidat.js
@@ -3,21 +3,21 @@ const mongoose = require("mongoose");
 const candidatSchema = new mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    postRecherche: { type: String, required: true },
+    PosteRecherche: { type: String, required: true },
     niveauExperience: { type: String, required: true },
     formationJuridique: { type: String, required: true },
     specialisations: { type: [String], default: null },
     domainExperiences: { type: [String], default: null },
     langues: [
       {
-        nom: { type: String, required: true },  
+        nom: { type: String, required: true },
         niveau: { type: String, required: true }
       }
     ],
-    typeTravailRecherche: {  type: String,  required: true},// Ex : "emploi", "stage"
+    typeTravailRecherche: { type: String, required: true },// Ex : "emploi", "stage"
     modeTravailRecherche: { type: String, required: true },
     villesTravailRecherche: { type: [String], required: true },
-    imageUrl: { type: String , default: null }
+    imageUrl: { type: String, default: null }
   },
   { timestamps: true }
 );
